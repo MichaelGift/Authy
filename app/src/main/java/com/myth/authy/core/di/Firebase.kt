19c9c769b5ba6415fd622f3a1a027db7c59b53ab
Michael Gift <simiyugift@outlook.com>
2024-04-02 19:42:20 +0300

    Provide firebase dependency
    - Firebase auth
    - Firebase firestore

---
+++ app/src/main/java/com/myth/authy/core/di/Firebase.kt
@@ -0,0 +1,27 @@
+package com.myth.authy.core.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Firebase {
+
+    @Provides
+    @Singleton
+    fun providesFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun providesFirebaseDatabase(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+}
